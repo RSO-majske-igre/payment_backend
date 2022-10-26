0ff654cfbbf23c04dfb3d68db6b8fdcabbb42670
JakMar17 <jakob.marusic17@gmail.com>
2022-10-26 12:36:54 +0200

    optimizing imports

--- database/src/main/java/team/marela/backend/database/repositories/invoice/InvoiceLineRepository.java
+++ database/src/main/java/team/marela/backend/database/repositories/invoice/InvoiceLineRepository.java
@@ -1,7 +1,6 @@
 package team.marela.backend.database.repositories.invoice;
 
 import team.marela.backend.database.BaseRepository;
-import team.marela.backend.database.entities.invoice.InvoiceEntity;
 import team.marela.backend.database.entities.invoice.InvoiceLineEntity;
 
 public interface InvoiceLineRepository extends BaseRepository<InvoiceLineEntity> {
