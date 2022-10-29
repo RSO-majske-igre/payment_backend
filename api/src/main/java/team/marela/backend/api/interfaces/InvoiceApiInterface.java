510ff7b651fb5546ecc65f8c5f61401a59d01dd9
JakMar17 <jakob.marusic17@gmail.com>
2022-10-29 11:39:09 +0200

    added openapi documentation

--- api/src/main/java/team/marela/backend/api/interfaces/InvoiceApiInterface.java
+++ api/src/main/java/team/marela/backend/api/interfaces/InvoiceApiInterface.java
@@ -1,6 +1,7 @@
 package team.marela.backend.api.interfaces;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -22,15 +23,37 @@ public interface InvoiceApiInterface {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Invoice created", content = @Content)
     })
-    InvoiceDto postInvoice(@RequestBody @Valid InvoiceDto invoice);
+    InvoiceDto postInvoice(
+            @RequestBody @Valid
+            @Parameter(description = "InvoiceDto to be saved")
+            InvoiceDto invoice
+    );
 
     @GetMapping("/{id}")
-    InvoiceDto getInvoiceById(@PathVariable UUID id);
+    @Operation(summary = "Returns invoice with given ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Invoice found nad returned", content = @Content)
+    })
+    InvoiceDto getInvoiceById(
+            @PathVariable
+            @Parameter(description = "Id of invoice to be found")
+            UUID id
+    );
 
     @GetMapping
+    @Operation(summary = "Returns invoices with pagination and sorting")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returned invoices with given pagination and sorting parameters", content = @Content)
+    })
     Page<InvoiceDto> getInvoices(
-            @RequestParam(required = false, defaultValue = "0")Integer page,
-            @RequestParam(required = false, defaultValue = "25") Integer perPage,
-            @RequestParam(required = false, defaultValue = "id") String sortBy
+            @RequestParam(required = false, defaultValue = "0")
+            @Parameter(description = "Page of data, starts with 0")
+            Integer page,
+            @RequestParam(required = false, defaultValue = "25")
+            @Parameter(description = "How many objects per response")
+            Integer perPage,
+            @RequestParam(required = false, defaultValue = "id")
+            @Parameter(description = "Sorting by")
+            String sortBy
     );
 }
